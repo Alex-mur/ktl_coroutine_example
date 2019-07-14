00f3eba2d03b4e4364c1d70e41a530471abd4eb7
Alexey <alex-mur@outlook.com>
2019-07-14 23:25:26 +0300

    k

--- app/src/main/java/my/test/kotlin_corutines_hw/ui/MainActivity.kt
+++ app/src/main/java/my/test/kotlin_corutines_hw/ui/MainActivity.kt
@@ -5,17 +5,20 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import my.test.kotlin_corutines_hw.R
+import my.test.kotlin_corutines_hw.model.repository.IRepo
 import my.test.kotlin_corutines_hw.model.repository.Repo
 import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
+    private val iRepo : IRepo = Repo
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         CoroutineScope(Dispatchers.Main + Job()).launch {
-            val launchList = Repo.getLastLaunches().await()
+            val launchList = iRepo.getLastLaunches().await()
             val sb : StringBuilder = StringBuilder()
             launchList.forEach{sb.append(it.mission_name)}
             test_text.text = sb.toString()
